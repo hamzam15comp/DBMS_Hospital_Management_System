ff7c1bb118a809ed87e3df2e1a7e524cd8351eb1
Pushan-Gore <pushan.gore@gmail.com>
2017-10-25 11:53:18 +0530

    Added new table style

--- login.php
+++ login.php
@@ -8,23 +8,117 @@
 <style type="text/css">
 
 <!--This is the table Css code-->
+body {
+    background: #fafafa url(https://jackrugile.com/images/misc/noise-diagonal.png);
+    color: #444;
+    font: 100%/30px 'Helvetica Neue', helvetica, arial, sans-serif;
+    text-shadow: 0 1px 0 #fff;
+}
+
+strong {
+    font-weight: bold; 
+}
+
+em {
+    font-style: italic; 
+}
 
 table {
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
+    background: #f5f5f5;
+    border-collapse: separate;
+    box-shadow: inset 0 1px 0 #fff;
+    font-size: 12px;
+    line-height: 24px;
+    margin: 30px auto;
+    text-align: left;
+    width: 800px;
+}   
+
+th {
+    background: url(https://jackrugile.com/images/misc/noise-diagonal.png), linear-gradient(#777, #444);
+    border-left: 1px solid #555;
+    border-right: 1px solid #777;
+    border-top: 1px solid #555;
+    border-bottom: 1px solid #333;
+    box-shadow: inset 0 1px 0 #999;
+    color: #fff;
+  font-weight: bold;
+    padding: 10px 15px;
+    position: relative;
+    text-shadow: 0 1px 0 #000;  
+}
+
+th:after {
+    background: linear-gradient(rgba(255,255,255,0), rgba(255,255,255,.08));
+    content: '';
+    display: block;
+    height: 25%;
+    left: 0;
+    margin: 1px 0 0 0;
+    position: absolute;
+    top: 25%;
     width: 100%;
 }
 
-td, th {
-    border: 1px solid #dddddd;
-    text-align: left;
-    padding: 8px;
+th:first-child {
+    border-left: 1px solid #777;    
+    box-shadow: inset 1px 1px 0 #999;
 }
 
-tr:nth-child(even) {
-    background-color: #dddddd;
+th:last-child {
+    box-shadow: inset -1px 1px 0 #999;
 }
 
+td {
+    border-right: 1px solid #fff;
+    border-left: 1px solid #e8e8e8;
+    border-top: 1px solid #fff;
+    border-bottom: 1px solid #e8e8e8;
+    padding: 10px 15px;
+    position: relative;
+    transition: all 300ms;
+}
+
+td:first-child {
+    box-shadow: inset 1px 0 0 #fff;
+}   
+
+td:last-child {
+    border-right: 1px solid #e8e8e8;
+    box-shadow: inset -1px 0 0 #fff;
+}   
+
+tr {
+    background: url(https://jackrugile.com/images/misc/noise-diagonal.png); 
+}
+
+tr:nth-child(odd) td {
+    background: #f1f1f1 url(https://jackrugile.com/images/misc/noise-diagonal.png); 
+}
+
+tr:last-of-type td {
+    box-shadow: inset 0 -1px 0 #fff; 
+}
+
+tr:last-of-type td:first-child {
+    box-shadow: inset 1px -1px 0 #fff;
+}   
+
+tr:last-of-type td:last-child {
+    box-shadow: inset -1px -1px 0 #fff;
+}   
+
+tbody:hover td {
+    color: transparent;
+    text-shadow: 0 0 3px #aaa;
+}
+
+tbody:hover tr:hover td {
+    color: #444;
+    text-shadow: 0 1px 0 #fff;
+}
+<!--
+-->
 /*!!!!!!!!!!! QuickMenu Core CSS [Do Not Modify!] !!!!!!!!!!!!!*/
 
 .qmmc .qmdivider{display:block;font-size:1px;border-width:0px;border-style:solid;}.qmmc .qmdividery{float:left;width:0px;}.qmmc .qmtitle{display:block;cursor:default;white-space:nowrap;}.qmclear {font-size:1px;height:0px;width:0px;clear:left;line-height:0px;display:block;float:none !important;}.qmmc {position:relative;zoom:1;}.qmmc a, .qmmc li {float:left;display:block;white-space:nowrap;}.qmmc div a, .qmmc ul a, .qmmc ul li {float:none;}.qmsh div a {float:left;}.qmmc div{visibility:hidden;position:absolute;}.qmmc ul {left:-10000px;position:absolute;}.qmmc, .qmmc ul {list-style:none;padding:0px;margin:0px;}.qmmc li a {float:none}.qmmc li{position:relative;}.qmmc ul {z-index:10;}.qmmc ul ul {z-index:20;}.qmmc ul ul ul {z-index:30;}.qmmc ul ul ul ul {z-index:40;}.qmmc ul ul ul ul ul {z-index:50;}li:hover>ul{left:auto;}#qm0 ul {top:100%;}#qm0 ul li:hover>ul{top:0px;left:100%;}
@@ -287,8 +381,8 @@ else if(isset($_GET['pid'])&&((strcmp($_GET['pid'],"view_doctors")==0))) {
         $count=mysqli_num_rows($result);
         //echo "<div style=\"width:990px; float:left\">";
         echo "<h3>Doctors</h3>";
-        echo "<table style=\"width:100%\";\"padding:15px\">
-                <tr>
+        echo "<table style=\"width:100%\";\"padding:15px\">";
+        echo "  <tr>
                 <th>Doctor Name</th>
                 <th>Employee ID</th>
                 <th>Department No.</th>
@@ -305,11 +399,11 @@ else if(isset($_GET['pid'])&&((strcmp($_GET['pid'],"view_doctors")==0))) {
             $dept=$row['Dept_No'];
             $add=$row['Address'];
             echo "  <tr>
-                    <th>$name</th>
-                    <th>$id</th>
-                    <th>$dept</th>
-                    <th>$add</th>
-                    <th>$contact</th>
+                    <td>$name</td>
+                    <td>$id</td>
+                    <td>$dept</td>
+                    <td>$add</td>
+                    <td>$contact</td>
                     </tr>";
             $count=$count-1;
         }
@@ -340,11 +434,11 @@ else if(isset($_GET['pid'])&&((strcmp($_GET['pid'],"view_admin")==0))) {
         $dept=$row['Dept_No'];
         $add=$row['Address'];
         echo "  <tr>
-                <th>$name</th>
-                <th>$id</th>
-                <th>$dept</th>
-                <th>$add</th>
-                <th>$contact</th>
+                <td>$name</td>
+                <td>$id</td>
+                <td>$dept</td>
+                <td>$add</td>
+                <td>$contact</td>
                 </tr>";
         $count=$count-1;
     }
@@ -373,11 +467,11 @@ else if(isset($_GET['pid'])&&((strcmp($_GET['pid'],"view_staff")==0))) {
         $dept=$row['Dept_No'];
         $add=$row['Address'];
         echo "  <tr>
-                <th>$name</th>
-                <th>$id</th>
-                <th>$dept</th>
-                <th>$add</th>
-                <th>$contact</th>
+                <td>$name</td>
+                <td>$id</td>
+                <td>$dept</td>
+                <td>$add</td>
+                <td>$contact</td>
                 </tr>";
         $count=$count-1;
     }
